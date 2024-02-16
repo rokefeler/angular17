45ff3969dadcb000513a02232557eba53e02979c
Jose Roque <rokefeler@gmail.com>
2024-02-15 21:03:11 -0500

    primer app angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todoapp';
+  welcome = 'Bendiciones este es tu primer programa';
+  tasks = [
+    'Instalar Angular CLI',
+    'Crear Proyecto',
+    'Crear Componentes',
+    'Crear Servicios'
+  ];
 }
